cedbd6df1b8ab878f693c703e2bed84b999ede17
Tomasz Wlodarczyk <tomek.wlod@gmail.com>
2021-09-03 11:57:30 +0100

    notify

--- notifier/notifier.js
+++ notifier/notifier.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const express = require("express");
 const app = express();
+const notifier = require("node-notifier");
 const port = process.env.PORT || 9000;
 
 app.use(express.json());
@@ -8,6 +10,29 @@ app.get("/health", (req, res) => {
   return res.status(200).send("ok");
 });
 
+app.post("/notify", (req, res) => {
+  notify(req.body, (reply) => res.send(reply));
+  // return res.status(200).send("ok");
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+const notify = ({ title, message }, cb) => {
+  notifier.notify(
+    {
+      title: title || "Unknown title",
+      message: message || "Unknown message",
+      icon: path.join(__dirname, "icon.png"),
+      sound: true,
+      wait: true,
+      reply: true,
+      closeLabel: "Completed?",
+      timeout: 15,
+    },
+    (err, response, reply) => {
+      cb(reply);
+    }
+  );
+};
